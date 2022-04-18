ee0ebb26cb5d2d95cfc7d038ff543006c96cdee1
ViktorG12345 <viktor.gamstorp.9778@student.uu.se>
2022-04-18 12:02:15 +0200

    kompetenser

--- backend/ikea/models.py
+++ backend/ikea/models.py
@@ -5,8 +5,18 @@ from django.db import models
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
-    info = models.BooleanField(default=False)
+    greeter = models.BooleanField(default=False)
     pins = models.BooleanField(default=False)
+    eco = models.BooleanField(default=False)
+    customer_service = models.BooleanField(default=False)
+    pins_responsible = models.BooleanField(default=False)
+    backoffice = models.BooleanField(default=False)
+    operative_responsible = models.BooleanField(default=False)
+    frontline = models.BooleanField(default=False)
+    smalland_1 = models.BooleanField(default=False)
+    smalland_2 = models.BooleanField(default=False)
+    count_kk = models.BooleanField(default=False)
+
 
     def _str_(self):
         return self.first_name
