c0640931f30debf4372f01b91822728731f2db67
EmilUnityGames <emilherre@gmail.com>
2022-04-19 14:52:10 +0200

    Startsida Bild+knappar

--- frontend/src/Components/Start.js
+++ frontend/src/Components/Start.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import startImg from './Viktor_omslag.png'
+import { useHistory } from 'react-router-dom';
 class Start extends React.Component {
+
+
     render(){
     return (
         <div>
             <img src={startImg} width="1000" height="500"/>
             <div>
-            <button className="GeneralButton"> All schedules </button>
+            <button className="GeneralButton" onClick={handleClick}> All schedules </button>
             <button className="GeneralButton"> New schedule </button>
             </div>
 
         </div>
         );
         }
-        }
+    };
+
+
 
 export default Start;
