219d1d2f0e63ac7069bf0c8981dcf73047954228
ChrilleLindberg <christoffer.lindberg.1806@student.uu.se>
2022-04-19 10:59:42 +0200

    openssl--

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
 import './App.css';
-import Create from './Components/Create';
 import Header from './Components/Header';
-import Start from './Components/Start';
 import MainContent from "./Components/MainContent";
 import Footer from "./Components/Footer";
 
 import axios from "axios";
 import React, { Component } from 'react';
-import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
         <Header />
         <MainContent />
-      <Routes>
-          <Route path="/" element={<Start />}></Route>
-          <Route path="/Create" element={<Create />}></Route>
-      </Routes>
         <Footer />
     </div>
   );
--- frontend/src/Components/MainContent.js
+++ frontend/src/Components/MainContent.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import Schedule from './Schedule';
 import Staff from './Staff';
 import Settings from './Settings';
+import Start from "./Start";
+import Create from "./Create";
+
+import {Route, Routes} from "react-router-dom";
+
+
 
 function MainContent() {
     return (
         <div>
-
+            <Routes>
+                <Route path="/" element={<Start />}></Route>
+                <Route path="/Create" element={<Create />}></Route>
+            </Routes>
         </div>
     );
 }
