292d5b4f446d14c65f66320870d628eda057f1a2
simonolausson <simon.olausson.8143@student.uu.se>
2022-04-20 22:03:35 +0200

    :)

--- frontend/src/Components/Start.js
+++ frontend/src/Components/Start.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import './Start.css';
-import startImg from "./Viktor_omslag.png";
+// import startImg from "./Viktor_omslag.png";
+import startImg from"./MaxiChrille.gif";
 class Start extends React.Component {
     render(){
     return (
         <div className="Body">
-            <img className="StartImg" src={startImg} width="1000" height="500"/>
+            <img className="StartImg" src={startImg} width="300" height="500"/>
             <div className="ButtonsDiv">
                 <Link to="/AllSchedules"><button className="AllButton">All schedules</button></Link>
                 <Link to="/Create"><button className="NewButton">New Schedule</button></Link>
