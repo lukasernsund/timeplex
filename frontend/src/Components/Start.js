aba99512b5fffdfd097143dfe0b19adf7dddca97
simonolausson <simon.olausson.8143@student.uu.se>
2022-04-20 12:21:28 +0200

    bild i start

--- frontend/src/Components/Start.js
+++ frontend/src/Components/Start.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './Start.css';
+import startImg from "./Viktor_omslag.png";
 class Start extends React.Component {
     render(){
     return (
         <div className="Body">
+            <img className="StartImg" src={startImg} width="1000" height="500"/>
             <div className="ButtonsDiv">
                 <button className="AllButton">All schedules</button>
                 <button className="NewButton">New Schedule</button>
