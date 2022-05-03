d353cdc92ec5a11941da05bfa18b011492df3659
simonolausson <simon.olausson.8143@student.uu.se>
2022-05-03 11:59:14 +0200

    login överst på sidan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Header from './Components/Header';
 import MainContent from "./Components/MainContent";
 import Footer from "./Components/Footer";
+import LoggaIn from './Components/LoggaIn';
 
 import axios from "axios";
 import React, { Component } from 'react';
 
+
 function App() {
   return (
     <div className="App">
+        <LoggaIn />
         <Header />
         <MainContent />
         <Footer />
