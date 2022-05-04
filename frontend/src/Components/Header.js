ed2ba772f8b5fd9e4d47a4a37bc71640ac6a6f0b
simonolausson <simon.olausson.8143@student.uu.se>
2022-05-04 14:42:02 +0200

    fixade header igen

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import Logo from './Timeplex.png';
 import Gear from './gear.png';
 import './Header.css';
+import { Link, NavLink } from 'react-router-dom';
+
+// function Header() {
+//     return (
+//         <header className="header">
+//                 <Link to="/"><img className="logo" src={Logo} alt={Logo}/></Link>
+//                 <ul className="nav">
+//                         <li className="list">
+//                             <Link className="link" to="/AllSchedules">All schedules</Link>
+//                         </li>
+//                         <li className="list">
+//                             <Link className="link" to="/Create">New schedule</Link>
+//                         </li>
+//                         <li className="list">
+//                             <Link className="link" to="/Employee">Employees</Link>
+//                         </li>
+//                         <li className="list">
+//                             <Link to="/Settings">
+//                                 <img className="gear" src={Gear} alt={Gear}/>
+//                             </Link>
+//                         </li>
+//                 </ul>
+//         </header>
+//     );
+// }
+
+// export default Header;
 
 function Header() {
     return (
         <header className="header">
-                <Link to="/"><img className="logo" src={Logo} alt={Logo}/></Link>
-                <ul className="nav">
-                        <li className="list">
-                            <Link className="link" to="/AllSchedules">All schedules</Link>
-                        </li>
-                        <li className="list">
-                            <Link className="link" to="/Create">New schedule</Link>
-                        </li>
-                        <li className="list">
-                            <Link className="link" to="/Employee">Employees</Link>
-                        </li>
-                        <li className="list">
-                            <Link to="/Settings">
-                                <img className="gear" src={Gear} alt={Gear}/>
-                            </Link>
-                        </li>
-                </ul>
+            <Link to="/"><img className="logo" src={Logo} alt={Logo}/></Link>
+            <NavLink to="/AllSchedules" activeClassName="active"> All Schedules </NavLink>
+            <NavLink to="/Create" activeClassName="active"> New Schedule </NavLink>            
+            <NavLink to="/Employee" activeClassName="active"> All Employees </NavLink>
+            <Link to="/Settings"><img className="gear" src={Gear} alt={Gear}/></Link>
         </header>
     );
 }
